22eae694106ec0f8fa53856e25a17d8cd2b36abe
José Conde Pinto <127633531+JosephusDev@users.noreply.github.com>
2025-01-28 01:46:21 -0300

    Initial commit

---
+++ src/styles/colors.ts
@@ -0,0 +1,14 @@
+export const colors = {
+  gray: {
+    100: '#FCFDFE',
+    200: '#E1EBF4',
+    300: '#C4D0DB',
+    400: '#73808C',
+    500: '#45525F',
+    600: '#1A1F24',
+  },
+  red: {
+    light: '#FF4C4C',
+    base: '#F94144',
+  },
+}
