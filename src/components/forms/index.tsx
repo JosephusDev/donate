d50485a5098b0af1974e795090e6e8acbd99bf6f
OliverOliveira <filomenoolivetree@gmail.com>
2025-02-03 16:30:03 +0100

    feat: backend connection

--- src/components/forms/index.tsx
+++ src/components/forms/index.tsx
@@ -1,4 +1,5 @@
 import HospitalForm from './hospital'
 import IndividualForm from './individual'
+import LoginForm from './login'
 
-export { HospitalForm, IndividualForm }
+export { HospitalForm, IndividualForm, LoginForm }
